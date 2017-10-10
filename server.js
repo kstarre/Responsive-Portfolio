d6ea9813300a2accae5eb30ba1b222ac5e58f4cb
Katie Starrett <starrett.k.e@gmail.com>
2017-10-10 17:07:06 -0400

    change into express app

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+let path = require('path');
+let favicon = require('serve-favicon');
+require('dotenv').config();
+
+// Set up express
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(express.static('./public'));
+app.use(favicon(path.join(__dirname, './public/img', 'favicon.ico')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+require("./routes/html-routes.js")(app);
